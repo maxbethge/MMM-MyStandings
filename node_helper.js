da304f2fcfe58af1c51c13fabfb703ef62b13b9e
Max Bethge <maxbethge@gmail.com>
2024-04-16 18:45:15 -0700

    Replaced request with axios

--- node_helper.js
+++ node_helper.js
@@ -3,7 +3,8 @@
  *
  */
 const NodeHelper = require("node_helper");
-const request = require('request');
+//const request = require('request');
+const axios = require("axios");
 const Log = require('../../js/logger.js');
 
 module.exports = NodeHelper.create({
@@ -15,18 +16,32 @@ module.exports = NodeHelper.create({
 	callUrl: function (notification, payload) {
 		var self = this;
 		Log.log('['+ this.name + '] ' + payload.instanceId + ' - callUrl -->  ' + notification);
-		request({ url: payload.url, method: 'GET' }, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var result = JSON.parse(body);
-				var newNotification = "STANDINGS_RESULT:" + notification.split(":")[1];
-				Log.log('['+ this.name + '] ' + payload.instanceId + ' - callUrl request succeeded, sending -> ' + newNotification);
-				self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: result});
-			} else {
-				Log.error('['+ this.name + '] ' + payload.instanceId + ' - could not load data, sending -> ' + newNotification + ', error: ' + error);
-				self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: null});
-			}
+		var newNotification = "STANDINGS_RESULT:" + notification.split(":")[1];
+		
+		//request({ url: payload.url, method: 'GET' }, function (error, response, body) {
+		//	if (!error && response.statusCode == 200) {
+		//		var result = JSON.parse(body);
+				
+		//		Log.log('['+ this.name + '] ' + payload.instanceId + ' - callUrl request succeeded, sending -> ' + newNotification);
+		//		self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: result});
+		//	} else {
+		//		Log.error('['+ this.name + '] ' + payload.instanceId + ' - could not load data, sending -> ' + newNotification + ', error: ' + error);
+		//		self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: null});
+		//	}
+		//});
+
+		axios.get(url)
+		.then( function(response) {
+				Log.log('['+ this.name + '] ' + payload.instanceId + ' - get request succeeded, sending -> ' + newNotification);
+				self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: response.data});
+		})
+		.catch( function(r_err) {
+			console.log( '['+ this.name + '] ' + payload.instanceId + " - " + moment().format("D-MMM-YY HH:mm") + " ** ERROR ** - " + r_err );
+			console.log( "[MMM-MyScoreboard] " + url );  
+			self.sendSocketNotification(newNotification, {instanceId: payload.instanceId, data: null});      
 		});
-	},
+
+		},
 
 	//Subclass socketNotificationReceived received.
 	socketNotificationReceived: function(notification, payload) {
